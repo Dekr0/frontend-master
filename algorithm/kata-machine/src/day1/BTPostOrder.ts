a55098ba3469b46a6bf7312f170e40fadc020f76
Dekr0 <dekr0.dk@gmail.com>
2023-05-22 13:15:37 -0600

    BT Traversal

--- algorithm/kata-machine/src/day1/BTPostOrder.ts
+++ algorithm/kata-machine/src/day1/BTPostOrder.ts
@@ -1,3 +1,11 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk<T>(curr: BinaryNode<T> | null, path: T[]): T[] {
+    if (!curr) return path;
+    walk(curr.left, path);
+    walk(curr.right, path);
+    path.push(curr.value);
+    return path;
+}
 
+export default function post_order_search<T>(head: BinaryNode<T>): T[] {
+    return walk(head, []);
 }
