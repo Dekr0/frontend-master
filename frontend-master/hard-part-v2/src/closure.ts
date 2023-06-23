f5f8c1da14d30374cc91e8f298e57e15e7cb109d
Dekr0 <dekr0.dk@gmail.com>
2023-06-22 23:06:34 -0600

    update

--- frontend-master/hard-part-v2/src/closure.ts
+++ frontend-master/hard-part-v2/src/closure.ts
@@ -25,9 +25,96 @@ function apply(f: Function, arg: any): Function | any {
 }
 
 function dateStamp(f: Function) {
-    return (...args: any[]) => { date: new Date(); output: f(...args) };
+    return (...args: any[]) => { return { date: new Date(), output: f(...args) } };
 }
 
-const stampedMultBy2 = dateStamp((n: number) => n * 2);
-console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
-console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+function censor() {
+    let pairs: {holder: string, replace: string}[] = [];
+
+    return function(first: string, second?: string): string | void {
+        if (second === undefined) {
+            pairs.forEach(e => first = first.replace(e.holder, e.replace))
+
+            return first;
+        } else {
+            pairs.push({
+                holder: first,
+                replace: second
+            });
+        }
+    }
+}
+
+function createSecretHolder<T>(secret: T) {
+    return {
+        getSecret: function(): T { return secret },
+        setSecret: (newSecret: T) => {
+            secret = newSecret;
+        }
+    }
+}
+
+function callTimes(f: Function) {
+    let counter = 0;
+
+    return (...args: any[]) => {
+        counter++;
+
+        return f(...args);
+    }
+}
+
+function roulette(num: number) {
+    return () => {
+        if (num-- > 0) {
+            return 'spin';
+        } else if (num-- === 0) {
+            return 'win';
+        } else {
+            return 'pick a number to play again';
+        }
+    }
+}
+
+function average() {
+    let sums: number[] = [];
+    let count = 0;
+    let avg: number = 0;
+
+    return (num?: number) => {
+        console.log(sums);
+        if (!num) {
+            return avg;
+        }
+
+        if (sums.length === 0) {
+            count++;
+            avg = num;
+            sums.push(num);
+
+            return avg;
+        } else {
+            const prev = sums.pop();
+            if (prev !== undefined) {
+                const next = prev + num;
+                sums.push(next);
+                count++;
+                avg = next / count; 
+
+                return avg;
+            } else {
+                throw new Error("???");
+            }
+        }
+    }
+}
+
+const avg = average();
+
+console.log(avg());
+console.log(avg(1));
+console.log(avg(2));
+console.log(avg(-3));
+console.log(avg());
+console.log(avg(5));
+console.log(avg());
